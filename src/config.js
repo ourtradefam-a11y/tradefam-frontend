612355423d44a26f72e33402cdb1c383f1649e05
Julia Geri <ourtradefam@gmail.com>
2025-11-15 21:13:45 +1000

    Fix API URL to use Railway backend

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://lucid-abundance-production.up.railway.app';
+export const AUTH0_DOMAIN = 'ourtradefam.au.auth0.com';
+export const AUTH0_CLIENT_ID = '2H7BTZfBOoQsmQ667uKaKmik8X4gKTNA';
+export const AUTH0_AUDIENCE = 'https://api.tradefam.app';
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://lucid-abundance-production.up.railway.app/api/v1';
 
 const api = {
   getPortfolios: (token) => {
